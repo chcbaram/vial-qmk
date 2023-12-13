fff796f0d473f98cb6daf1f96b92327d610d2a50
Hancheol Cho <chcbaram@gmail.com>
2023-12-13 15:16:56 +0900

    TODORO-81K 키보드 추가

---
+++ keyboards/todoro/81k/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x52, 0xFB, 0x61, 0xF8, 0xB7, 0xD5, 0x08, 0x11}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 5}
+#define VIAL_UNLOCK_COMBO_COLS {0, 6}
