b3bff29973e73c5e2e847390605afdf94bb1f992
Hancheol Cho <chcbaram@gmail.com>
2023-12-16 11:44:04 +0900

    baram-45k 레이어 8개로 변경

--- keyboards/baram/45k/keymaps/vial/config.h
+++ keyboards/baram/45k/keymaps/vial/config.h
@@ -2,6 +2,9 @@
 
 #pragma once
 
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT  8
+
 #define VIAL_KEYBOARD_UID {0x3B, 0xE9, 0xBB, 0xEE, 0x96, 0x19, 0xDD, 0x96}
 #define VIAL_UNLOCK_COMBO_ROWS {0, 1}
 #define VIAL_UNLOCK_COMBO_COLS {0, 11}
