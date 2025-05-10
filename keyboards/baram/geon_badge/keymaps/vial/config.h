a98783037f4f13d742061e7a195ef7f5988fc0b8
Hancheol Cho <chcbaram@gmail.com>
2025-05-11 08:44:28 +0900

    geon-badge 추가

---
+++ keyboards/baram/geon_badge/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT  4
+
+#define VIAL_KEYBOARD_UID {0x81, 0x9C, 0x94, 0xFC, 0xBE, 0x68, 0x15, 0x12}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 2}
---
+++ keyboards/baram/geon_badge/rules.mk
@@ -0,0 +1,2 @@
+# This file intentionally left blank
+KEY_OVERRIDE_ENABLE = yes     # Enable Key Overrides, see https://docs.qmk.fm/#/feature_key_overrides
