24cbe6d785030f6dcb27f8c97e34757c079488f2
Hancheol Cho <chcbaram@gmail.com>
2025-05-27 21:39:43 +0900

    BARAM NUMPAD 추가

---
+++ keyboards/baram/numpad/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT  8
+
+#define VIAL_KEYBOARD_UID {0xCE, 0xE4, 0x71, 0x1E, 0x50, 0xFF, 0x90, 0x69}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 3}
