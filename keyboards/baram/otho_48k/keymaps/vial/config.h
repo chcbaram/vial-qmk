e288f3aeeb83c0260672c5679a7be07ca5c1bd35
Hancheol Cho <chcbaram@gmail.com>
2025-06-03 08:59:57 +0900

    BARAM OTHO 48K 추가

---
+++ keyboards/baram/otho_48k/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT  8
+
+#define VIAL_KEYBOARD_UID {0x87, 0x26, 0xE7, 0xC2, 0x92, 0xE5, 0x10, 0xE3}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 3}
