3a00a98b048d4e3867087cf73bea0dd72ecf2b75
Hancheol Cho <chcbaram@gmail.com>
2026-03-02 18:11:54 +0900

    ERGO60 추가

---
+++ keyboards/baram/baram/ergo60/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT  8
+
+#define VIAL_KEYBOARD_UID {0x21, 0xCF, 0x5D, 0xCB, 0xE7, 0x8C, 0xF3, 0x46}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 4}
+#define VIAL_UNLOCK_COMBO_COLS {0, 0}
