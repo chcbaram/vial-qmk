1e996d08ad85bd767ca18cd0b4a268220ef20d74
Hancheol Cho <chcbaram@gmail.com>
2026-03-16 22:58:24 +0900

    RAMUNE60 추가

---
+++ keyboards/baram/baram/wish60_lp/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT  8
+
+#define VIAL_KEYBOARD_UID {0xAF, 0x82, 0x05, 0x25, 0x9C, 0x51, 0x32, 0x6E}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 4}
+#define VIAL_UNLOCK_COMBO_COLS {0, 0}
---
+++ keyboards/baram/geon/ramune60/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT  8
+
+#define VIAL_KEYBOARD_UID {0x5E, 0x15, 0xC6, 0xDD, 0x35, 0xA1, 0xF6, 0xB0}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 4}
+#define VIAL_UNLOCK_COMBO_COLS {0, 0}
